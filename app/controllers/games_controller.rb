64f5f957969785978eaca4f87883b0cf3ec35fa4
Clayton Flaherty <claytonflaherty92@gmail.com>
2022-08-15 17:16:23 +0200

    game complete and partially styled

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,38 @@
+require "json"
+require "open-uri"
+
+class GamesController < ApplicationController
+  def new
+    charset = Array('A'..'Z')
+    @letters = Array.new(10) { charset.sample }
+  end
+
+  def score
+    @response = params['response']
+    check = params['letters'].split()
+    original = params['letters'].split()
+    split_response = @response.split("")
+    new_array = []
+    copy_array = check
+    split_response.each do |letter|
+      new_array << letter.upcase
+    end
+    valid = []
+    new_array.each do |letter|
+      if check.include?(letter)
+        valid << 'yes'
+        check.delete(letter)
+      end
+    end
+    filepath = "https://wagon-dictionary.herokuapp.com/#{@response}"
+    serialized_score = URI.open(filepath).read
+    @score_check = JSON.parse(serialized_score)
+    if @score_check['found'] == false
+      @comment = "Sorry #{@response} is not a valid english word."
+    elsif @score_check['found'] == true && valid != [] && valid.length == new_array.length
+      @comment = "Congratulations! #{@response} is a valid English word."
+    elsif @score_check['found'] == true
+      @comment = "Sorry, #{@response} cannot be built out of #{original.join('')}"
+    end
+  end
+end
